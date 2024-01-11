9419b7694bec24fa1d47f04dcfaa5d9e03dc6748
Douglas <dougl215@sdf-login02.slac.stanford.edu>
2024-01-11 14:51:06 -0800

    added slurm batch directive, alternative ID scheme

--- runFullChain.sh
+++ runFullChain.sh
@@ -11,18 +11,25 @@
 #SBATCH --gpus=v100:1
 #
 #SBATCH --time=10:00:00
+#
+#SBATCH --array=1-100
 
 SINGULARITY_IMAGE_PATH=/sdf/group/neutrino/images/larcv2_ub20.04-cuda11.3-cudnn8-pytorch1.10.0-larndsim-2022-11-03.sif
 
 # OUTDIR=/sdf/group/neutrino/dougl215/singleParticle/singleParticle_3GeV
 OUTDIR=$1
-UUID=$(cat /proc/sys/kernel/random/uuid)
+
+# I've been using a UUID from the OS to make unique filenames
+# these are very long and can be quite ugly
+# you can also just use the slurm batch index
+ID=$(cat /proc/sys/kernel/random/uuid) 
+#ID=$SLURM_ARRAY_TASK_ID # this will just assume a the index of the slurm job (less safe to being clobbered by new outputs!)
 
 G4MACRO=$2
 
 EDEPDIR=${OUTDIR}/edep-sim
 mkdir -p $EDEPDIR
-EDEPOUTPUT=${EDEPDIR}/edep_single_particle_${UUID}.root
+EDEPOUTPUT=${EDEPDIR}/edep_single_particle_${ID}.root
 
 # COMMAND="edep-sim -g nd_hall_only_lar_TRUE_1.gdml -e 1000 -o ${EDEPOUTPUT} g4.mac"
 # COMMAND="edep-sim -g nd_hall_only_lar_TRUE_1.gdml -e 1000 -o ${EDEPOUTPUT} g4_stoppingMuon.mac"
@@ -36,7 +43,7 @@ singularity exec -B /sdf,/scratch,/lscratch ${SINGULARITY_IMAGE_PATH} ${COMMAND}
 
 DUMPTREEDIR=${OUTDIR}/dumpTree
 mkdir $DUMPTREEDIR
-DUMPTREEOUTPUT=${DUMPTREEDIR}/dumpTree_single_particle_${UUID}.h5
+DUMPTREEOUTPUT=${DUMPTREEDIR}/dumpTree_single_particle_${ID}.h5
 
 COMMAND="python3 dumpTree.py ${EDEPOUTPUT} ${DUMPTREEOUTPUT}"
 
